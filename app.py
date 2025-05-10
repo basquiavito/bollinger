7fe3d020c04c506d419a345a0f71cd6bce6cde60
rclemon <richardg@imac.mynetworksettings.com>
2025-05-10 13:43:46 -0400

    bollinger 2

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import bollinger  # This assumes bollinger.py is in the same directory
 
 def main():
-    st.set_page_config(page_title="Bollinger Bands Viewer", layout="wide")
     bollinger.main()
 
 if __name__ == "__main__":
