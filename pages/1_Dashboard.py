0c9de78002d8fbd5e9bf9022bcbe63becb94fdcd
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-05-28 23:35:20 -0400

    Update 1_Dashboard.py
    airplane back

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -3820,33 +3820,33 @@ if st.sidebar.button("Run Analysis"):
 
 
 
-#                     # ✈️ Upward Crosses ABOVE Yesterday High
-#                     up_high_mask = intraday["Y_High_Cross"] == "✈️"
-#                     up_high_trace = go.Scatter(
-#                         x=intraday.loc[up_high_mask, "Time"],
-#                         y=intraday.loc[up_high_mask, "F_numeric"] + 144,
-#                         mode="text",
-#                         text=intraday.loc[up_high_mask, "Y_High_Cross"],
-#                         textposition="top center",
-#                         textfont=dict(size=55),
-#                         name="Cross Above Y-High (✈️)"
-#                     )
+                    # ✈️ Upward Crosses ABOVE Yesterday High
+                    up_high_mask = intraday["Y_High_Cross"] == "✈️"
+                    up_high_trace = go.Scatter(
+                        x=intraday.loc[up_high_mask, "Time"],
+                        y=intraday.loc[up_high_mask, "F_numeric"] + 144,
+                        mode="text",
+                        text=intraday.loc[up_high_mask, "Y_High_Cross"],
+                        textposition="top center",
+                        textfont=dict(size=55),
+                        name="Cross Above Y-High (✈️)"
+                    )
 
-#                     # 🪂 Downward Crosses BELOW Yesterday High
-#                     down_high_mask = intraday["Y_High_Cross"] == "🪂"
-#                     down_high_trace = go.Scatter(
-#                         x=intraday.loc[down_high_mask, "Time"],
-#                         y=intraday.loc[down_high_mask, "F_numeric"] - 144,
-#                         mode="text",
-#                         text=intraday.loc[down_high_mask, "Y_High_Cross"],
-#                         textposition="bottom center",
-#                         textfont=dict(size=55),
-#                         name="Cross Below Y-High (🪂)"
-#                     )
+                    # 🪂 Downward Crosses BELOW Yesterday High
+                    down_high_mask = intraday["Y_High_Cross"] == "🪂"
+                    down_high_trace = go.Scatter(
+                        x=intraday.loc[down_high_mask, "Time"],
+                        y=intraday.loc[down_high_mask, "F_numeric"] - 144,
+                        mode="text",
+                        text=intraday.loc[down_high_mask, "Y_High_Cross"],
+                        textposition="bottom center",
+                        textfont=dict(size=55),
+                        name="Cross Below Y-High (🪂)"
+                    )
 
-#                     # Add to figure
-#                     fig.add_trace(up_high_trace, row=1, col=1)
-#                     fig.add_trace(down_high_trace, row=1, col=1)
+                    # Add to figure
+                    fig.add_trace(up_high_trace, row=1, col=1)
+                    fig.add_trace(down_high_trace, row=1, col=1)
 
 
 
