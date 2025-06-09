a1111b5d017a84a9be4cf9c19efd62e243cf6033
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-06-09 13:30:19 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -24,7 +24,7 @@ st.title("VOLMIKE.COM")
 # ======================================
 st.sidebar.header("Input Options")
 
-default_tickers = ["SPY","TSLA","QQQ","NVDA","SMCI", "AVGO","AMD","PLTR","MRVL","uber","AMZN","AAPL","googl","META","MSFT","MARA","nke","GM","c","wfc","hood","coin","bac","jpm","HIMS","TXM","QCOM","MU","INTC","CRDO","RMBS","ON","ORCL", "CRWD","PANW","APP","MSTR","IBM","AMAT","DELL","WDC","CRM","CHWY","ETSY","CART","W"]
+default_tickers = ["SPY","QQQ","NVDA","SMCI", "AVGO","AMD","PLTR","MRVL","uber","AMZN","AAPL","googl","META","MSFT","MARA","nke","GM","c","wfc","hood","coin","bac","jpm","HIMS","TXM","QCOM","MU","INTC","CRDO","RMBS","ON","ORCL", "CRWD","PANW","APP","MSTR","IBM","AMAT","DELL","WDC","CRM","CHWY","ETSY","CART","W"]
 tickers = st.sidebar.multiselect(
     "Select Tickers",
     options=default_tickers,
