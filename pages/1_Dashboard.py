a6fed2f6e87e059623ba14ec3ce532600c113927
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-06-28 09:08:14 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -3863,7 +3863,7 @@ if st.sidebar.button("Run Analysis"):
                 fig.update_layout(
                     title=f"{t} – VOLMIKE.COM",
                     margin=dict(l=30, r=30, t=50, b=30),
-                    height=2000,  # Increase overall figure height (default ~450-600)
+                    height=1200,  # Increase overall figure height (default ~450-600)
 
                     showlegend=True,
                     yaxis2=dict(
