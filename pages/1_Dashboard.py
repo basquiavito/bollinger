9d322ba44b72bdc60f50ebfb1a36fb28ea5f995f
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-06-28 22:45:33 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -4311,7 +4311,7 @@ if st.sidebar.button("Run Analysis"):
   
                 fig.add_trace(go.Scatter(
                     x=emoji_df["TimeIndex"],
-                    y=emoji_df["F_numeric"] - 13,
+                    y=emoji_df["F_numeric"] - 18,
                     mode="text",
                     text=emoji_df["Mike_Kijun_Bee_Emoji"],
                     textposition="top center",
