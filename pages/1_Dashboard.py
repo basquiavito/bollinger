38c8237b01ea39bff081197edb670fde6c23be46
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-06-28 20:16:27 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -4220,8 +4220,7 @@ if st.sidebar.button("Run Analysis"):
                     margin=dict(l=30, r=30, t=50, b=30),
                     height=1200,  # Increase overall figure height (default ~450-600)
 
-                    showlegend=True,
-                    
+                     
                 )
 
             
