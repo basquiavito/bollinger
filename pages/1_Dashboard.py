92c9483d3c31f6362feb2b7551341fa288ff2f53
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-07-01 22:15:41 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -4903,8 +4903,7 @@ if st.sidebar.button("Run Analysis"):
                         annotation_text="🦻🏼",
                         annotation_position="top left",
                         annotation_font=dict(color="black"),
-                        hoverlabel=dict(bgcolor="white"),
-                        hovertemplate=f"🦻🏼 Ear Shift<br>%Vol: {vol:.2f}<br>Time: {time}<extra></extra>"
+              
                     )
 
   
