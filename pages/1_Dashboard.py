d52a545a6615dc191a9f460477269e56d4fee9d7
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-07-04 12:49:11 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -5666,16 +5666,16 @@ if st.sidebar.button("Run Analysis"):
   
           
   
-                          
-            if yva_min is not None and yva_max is not None:
-                # Show in text
-                st.markdown(f"**📘 Yesterday’s Value Area**: {yva_min} → {yva_max}")
-            if prev_close:
-                range_f_pct = round((prev_high - prev_low) / prev_close * 100, 1)
-                st.markdown(f"📏 Yesterday’s Range: **{prev_low:.2f} → {prev_high:.2f}** ({yesterday_range_str} pts | {range_f_pct}%)")
-   
+                              
+                if yva_min is not None and yva_max is not None:
+                    # Show in text
+                    st.markdown(f"**📘 Yesterday’s Value Area**: {yva_min} → {yva_max}")
+                if prev_close:
+                    range_f_pct = round((prev_high - prev_low) / prev_close * 100, 1)
+                    st.markdown(f"📏 Yesterday’s Range: **{prev_low:.2f} → {prev_high:.2f}** ({yesterday_range_str} pts | {range_f_pct}%)")
+       
+      
 
-  
   
 
 
