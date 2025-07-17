d0764a4daba608fecadf065a6fc17dd995f5f308
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-07-17 19:12:41 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -4727,32 +4727,18 @@ if st.sidebar.button("Run Analysis"):
                     )
 
 
-                    #                   # (E) Compliance Shift Bubbles on Main Plot
-                    shift_bubbles = go.Scatter(
-                        x=intraday["Time"],
-                        showlegend=False,
-
-                        y=intraday["F%"].where(intraday["Compliance Shift"] == "🫧") + 50,
-                        mode="markers",
-                        marker=dict(size=30, symbol="circle", color="#00ccff", line=dict(color="white", width=1)),
-                        name="🫧 Compliance Shift",
-                    )
-                    
-                    # (E) Compliance Shift Bubbles on Main Plot
+                    # #                   # (E) Compliance Shift Bubbles on Main Plot
                     # shift_bubbles = go.Scatter(
                     #     x=intraday["Time"],
-                    #     y=pd.to_numeric(intraday["F%"], errors="coerce").where(intraday["Compliance Shift"] == "🫧") + 50,
-                    #     mode="markers",
                     #     showlegend=False,
+
+                    #     y=intraday["F%"].where(intraday["Compliance Shift"] == "🫧") + 50,
+                    #     mode="markers",
                     #     marker=dict(size=30, symbol="circle", color="#00ccff", line=dict(color="white", width=1)),
                     #     name="🫧 Compliance Shift",
-                    #     hovertemplate="Time: %{x|%H:%M}<br>Compliance: %{y:.2f}<extra></extra>"
                     # )
                     
-                    # fig.add_trace(shift_bubbles, row=1, col=1)
-
-                    # Add all traces
-
+                   
                     fig.add_trace(upper_band, row=1, col=1)
                     fig.add_trace(lower_band, row=1, col=1)
                     fig.add_trace(middle_band, row=1, col=1)
