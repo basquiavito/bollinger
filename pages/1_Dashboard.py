2eab8fbe4d69b3c066721ae3e458137699e053c6
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-07-16 23:42:13 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -6245,50 +6245,50 @@ if st.sidebar.button("Run Analysis"):
 
 
   
-                # # intraday["SpanA_F"] = ((intraday["SpanA"] - prev_close) / prev_close) * 10000
-                # # intraday["SpanB_F"] = ((intraday["SpanB"] - prev_close) / prev_close) * 10000
+                # intraday["SpanA_F"] = ((intraday["SpanA"] - prev_close) / prev_close) * 10000
+                # intraday["SpanB_F"] = ((intraday["SpanB"] - prev_close) / prev_close) * 10000
   
   
   
-                #                     # Span A – Yellow Line
-                # span_a_line = go.Scatter(
-                #     x=intraday["Time"],
-                #     y=intraday["SpanA_F"],
-                #     mode="lines",
-                #     line=dict(color="yellow", width=0.4),
-                #     name="Span A (F%)"
-                # )
-                # fig.add_trace(span_a_line, row=1, col=1)
+                                    # Span A – Yellow Line
+                span_a_line = go.Scatter(
+                    x=intraday["Time"],
+                    y=intraday["SpanA_F"],
+                    mode="lines",
+                    line=dict(color="yellow", width=0.4),
+                    name="Span A (F%)"
+                )
+                fig.add_trace(span_a_line, row=1, col=1)
   
-                # # Span B – Blue Line
-                # span_b_line = go.Scatter(
-                #     x=intraday["Time"],
-                #     y=intraday["SpanB_F"],
-                #     mode="lines",
-                #     line=dict(color="rgba(0, 150, 255, 0.4)", width=0.5),                    
-                #     name="Span B (F%)"
-                # )
-                # fig.add_trace(span_b_line, row=1, col=1)
+                # Span B – Blue Line
+                span_b_line = go.Scatter(
+                    x=intraday["Time"],
+                    y=intraday["SpanB_F"],
+                    mode="lines",
+                    line=dict(color="rgba(0, 150, 255, 0.4)", width=0.5),                    
+                    name="Span B (F%)"
+                )
+                fig.add_trace(span_b_line, row=1, col=1)
   
-                # # Invisible SpanA for cloud base
-                # fig.add_trace(go.Scatter(
-                #     x=intraday["Time"],
-                #     y=intraday["SpanA_F"],
-                #     line=dict(width=0),
-                #     mode='lines',
-                #     showlegend=False
-                # ), row=1, col=1)
+                # Invisible SpanA for cloud base
+                fig.add_trace(go.Scatter(
+                    x=intraday["Time"],
+                    y=intraday["SpanA_F"],
+                    line=dict(width=0),
+                    mode='lines',
+                    showlegend=False
+                ), row=1, col=1)
   
-                # # SpanB with fill → grey Kumo
-                # fig.add_trace(go.Scatter(
-                #     x=intraday["Time"],
-                #     y=intraday["SpanB_F"],
-                #     fill='tonexty',
-                #     fillcolor='rgba(20, 20, 30, 0.10)',  # transparent grey
-                #     line=dict(width=0),
-                #     mode='lines',
-                #     name='Kumo Cloud'
-                # ), row=1, col=1)
+                # SpanB with fill → grey Kumo
+                fig.add_trace(go.Scatter(
+                    x=intraday["Time"],
+                    y=intraday["SpanB_F"],
+                    fill='tonexty',
+                    fillcolor='rgba(20, 20, 30, 0.10)',  # transparent grey
+                    line=dict(width=0),
+                    mode='lines',
+                    name='Kumo Cloud'
+                ), row=1, col=1)
 
                 # # if yva_min is not None and yva_max is not None:
                 #     # Show in text
