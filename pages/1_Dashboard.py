5bd71dddfd57e48f0a11ec09b2a2fca79f12f7cc
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-07-21 23:25:52 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -6636,7 +6636,7 @@ if st.sidebar.button("Run Analysis"):
   
                 fig.add_trace(go.Scatter(
                     x=intraday.loc[cloud_mask, "Time"],
-                    y=intraday.loc[cloud_mask, "F_numeric"] +24,
+                    y=intraday.loc[cloud_mask, "F_numeric"] +44,
                     mode="text",
                     text=intraday.loc[cloud_mask, "Heaven_Cloud"],
                     textposition="top center",
@@ -6668,7 +6668,7 @@ if st.sidebar.button("Run Analysis"):
   
                 fig.add_trace(go.Scatter(
                     x=intraday.loc[drizzle_mask, "Time"],
-                    y=intraday.loc[drizzle_mask, "F_numeric"] + 24,  # Position below the bar
+                    y=intraday.loc[drizzle_mask, "F_numeric"] + 44,  # Position below the bar
                     mode="text",
                     text=intraday.loc[drizzle_mask, "Drizzle_Emoji"],
                     textposition="bottom center",
