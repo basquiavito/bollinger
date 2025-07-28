591d253b464bf17dce10650f6f0984f32f941039
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-07-28 18:40:13 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -6225,7 +6225,7 @@ if st.sidebar.button("Run Analysis"):
                     fig.update_yaxes(title_text="Volatility", row=2, col=1)
                     fig.update_xaxes(title_text="Time", row=2, col=1)
                                                   
-                st.plotly_chart(fig_displacement, use_container_width=True)
+                st.plotly_chart(fig, use_container_width=True)
             
                          
                
