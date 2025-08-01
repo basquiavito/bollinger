ca0ec5aa44c7e9ad5b80689738b849765d415c79
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-08-01 01:11:02 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -8944,18 +8944,18 @@ if st.sidebar.button("Run Analysis"):
                 ), row=1, col=1)
 
     
-               fig.add_trace(go.Scatter(
-                   x=compliance_aid_times,
-                   y=compliance_aid_prices,
-                   mode="text",
-                   text=["🫧"] * len(compliance_aid_times),
-                   textposition="top center",
-                   textfont=dict(size=21),
-                   name="Compliance Shift Aid 🫧",
-                   hovertemplate="Time: %{x}<br>Compliance Support Nearby<extra></extra>"
-              ),   row=1, col=1)
-          
-          
+                 fig.add_trace(go.Scatter(
+                     x=compliance_aid_times,
+                     y=compliance_aid_prices,
+                     mode="text",
+                     text=["🫧"] * len(compliance_aid_times),
+                     textposition="top center",
+                     textfont=dict(size=21),
+                     name="Compliance Shift Aid 🫧",
+                     hovertemplate="Time: %{x}<br>Compliance Support Nearby<extra></extra>"
+                ),   row=1, col=1)
+            
+            
 
 
                 fig.update_yaxes(title_text="Option Value", row=2, col=1)
