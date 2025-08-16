81bbb6db45db43eaf95fc4fd98a89f4736b6f9f1
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-08-16 14:19:22 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -4888,7 +4888,18 @@ if st.sidebar.button("Run Analysis"):
                 # Usage:
                 intraday = calculate_n_wilder(intraday)
                 
-
+                
+                def detect_n_compression(intraday, lookback=5, threshold=0.7):
+                    """
+                    Assigns 🐝 when N compresses below a threshold relative to N[lookback bars ago]
+                    """
+                    intraday['N_Ratio_Compress'] = intraday['N'] / intraday['N'].shift(lookback)
+                    intraday['N_Compress_Alert'] = ''
+                
+                    intraday.loc[intraday['N_Ratio_Compress'] <= threshold, 'N_Compress_Alert'] = '🐝'
+                    
+                    return intraday
+                intraday = detect_n_compressionr(intraday)
 
 
 
@@ -4977,7 +4988,7 @@ if st.sidebar.button("Run Analysis"):
                 with st.expander("Show/Hide Data Table",  expanded=False):
                                 # Show data table, including new columns
                     cols_to_show = [
-                                    "RVOL_5","Range",'N',"Time","Volume","Sharpe_Ratio","Call_BBW_Tight_Emoji","Put_BBW_Tight_Emoji","Compliance","Distensibility","Distensibility Alert","Volatility_Composite","Gravity_Break_Alert","F_numeric","Kijun_Cumulative","Unit%","Vector%","Unit Velocity","Velocity","Voltage","Vector_Charge","Vector_Capacitance","Charge_Polarity","Field_Intensity","Electric_Force","Unit Acceleration","Acceleration","Accel_Spike","Acceleration_Alert","Jerk_Unit","Jerk_Vector","Snap","Unit Momentum","Vector Momentum","Unit Force","Vector Force","Power","Intensity","Unit Energy","Vector Energy","Force_per_Range","Force_per_3bar_Range","Unit_Energy_per_Range","Vector_Energy_per_3bar_Range"]
+                                    "RVOL_5","Range",'N','N_Ratio_Compress','N_Compress_Alert',"Time","Volume","Sharpe_Ratio","Call_BBW_Tight_Emoji","Put_BBW_Tight_Emoji","Compliance","Distensibility","Distensibility Alert","Volatility_Composite","Gravity_Break_Alert","F_numeric","Kijun_Cumulative","Unit%","Vector%","Unit Velocity","Velocity","Voltage","Vector_Charge","Vector_Capacitance","Charge_Polarity","Field_Intensity","Electric_Force","Unit Acceleration","Acceleration","Accel_Spike","Acceleration_Alert","Jerk_Unit","Jerk_Vector","Snap","Unit Momentum","Vector Momentum","Unit Force","Vector Force","Power","Intensity","Unit Energy","Vector Energy","Force_per_Range","Force_per_3bar_Range","Unit_Energy_per_Range","Vector_Energy_per_3bar_Range"]
 
                     st.dataframe(intraday[cols_to_show])
 
