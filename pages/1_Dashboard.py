a85f038823c5efe6c51dc62c33499ce12d344a3c
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-08-18 22:05:04 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -9464,14 +9464,21 @@ if st.sidebar.button("Run Analysis"):
                     range_f_pct = round((prev_high - prev_low) / prev_close * 100, 1)
                     st.markdown(f"📏 Yesterday’s Range: **{prev_low:.2f} → {prev_high:.2f}** ({yesterday_range_str} pts | {range_f_pct}%)")
 
-
-                            # Check and display YVA only
+   # # 🧭 Opening Position vs YVA
                 if yva_min is not None and yva_max is not None:
-                    st.markdown(f"**📘 Yesterday’s Value Area**: {yva_min:,.2f} → {yva_max:,.2f}")
-                else:
-                    st.markdown("**📘 Yesterday’s Value Area**: —")
+                    opening_price = intraday["Close"].iloc[0]
                 
-
+                if yva_min < opening_price < yva_max:
+                        yva_position_msg = "✅ Opened **within** Yesterday's Value Area"
+                    elif opening_price >= yva_max:
+                        yva_position_msg = "⬆️ Opened **above** Yesterday's Value Area"
+                    elif opening_price <= yva_min:
+                        yva_position_msg = "⬇️ Opened **below** Yesterday's Value Area"
+                    else:
+                        yva_position_msg = "⚠️ Could not determine opening position relative to YVA"
+                
+                    st.markdown(f"### {yva_position_msg}")
+            
                 fig.update_layout(
                     title=f"VOLMIKE.COM  - {start_date.strftime('%Y-%m-%d')}",
                     margin=dict(l=30, r=30, t=50, b=30),
