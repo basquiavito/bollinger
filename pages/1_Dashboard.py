8f7c4a30ec2286ddc340ae3f5643daf7f7956d57
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-09-23 10:11:56 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -11035,7 +11035,7 @@ if st.sidebar.button("Run Analysis"):
                     title=f"VOLMIKE.COM  - {start_date.strftime('%Y-%m-%d')}",
                     margin=dict(l=30, r=30, t=50, b=30),
                     height=2500,  # Increase overall figure height (default ~450-600)
-                    showlegend=True,
+                    showlegend=False,
                 
 
                      
