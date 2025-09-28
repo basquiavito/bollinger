c51c19fb8e31f7c8214dee2377e53f8893a70fbc
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-09-28 13:16:54 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -7765,9 +7765,10 @@ if st.sidebar.button("Run Analysis"):
                     map_parallel_after_t2, axis=1, args=(intraday,)
                      )
 
-                    df[["Goldmine_E2_Emoji", "Goldmine_E2_Time", "Goldmine_E2 Price ($)"]] = df.apply(
+                    df[["Goldmine_Emoji", "Goldmine_Time", "Goldmine Price ($)"]] = df.apply(
                     map_goldmine_after_e2, axis=1, args=(intraday,), result_type="expand"
-                    )
+                     )
+
 
                     df[["Goldmine_Emoji", "Goldmine_Time", "Goldmine Price ($)"]] = df.apply(
                     map_goldmine_after_t1, axis=1, args=(intraday,), result_type="expand"
