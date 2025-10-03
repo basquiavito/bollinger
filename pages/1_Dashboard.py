5fbfb9a099b8916636b9a13e16676c071653233f
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-10-02 22:23:44 -0400

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -7730,7 +7730,7 @@ if st.sidebar.button("Run Analysis"):
                     for i in intraday.index[intraday["Put_FirstEntry_Emoji"] == "🎯"]:
                         entries.append({"Type": "Put 🎯1",
                                         "Time": pd.to_datetime(intraday.at[i, "Time"]).strftime("%H:%M"),
-                                        "Price ($) ": intraday.at[i, "Close"],
+                                        "Price ($)": intraday.at[i, "Close"],
                                         "F%": intraday.at[i, "F_numeric"],   # works for every row
 })
                     for i in intraday.index[intraday["Put_SecondEntry_Emoji"] == "🎯2"]:
