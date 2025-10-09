b029dca61f1ea3bbd247bc54953e9ba6816f48bb
basquiavito <54229601+basquiavito@users.noreply.github.com>
2025-10-08 21:13:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
-from notify import send_text
 
-# 🔥 QUICK TEST — remove these 2 lines after you see the text
-send_text("AMD", "CALL", "220.85", "10:30")
 
 st.set_page_config(page_title="Options Wealth", layout="wide")
 
